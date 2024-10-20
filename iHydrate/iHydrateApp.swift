68caa19d8562741752141a5f6d11668fa6004b27
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-10-20 14:31:27 +0300

    Initial Commit

---
+++ iHydrate/iHydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHydrateApp.swift
+//  iHydrate
+//
+//  Created by Haya Albaker on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iHydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iHydrateTests/iHydrateTests.swift
@@ -0,0 +1,17 @@
+//
+//  iHydrateTests.swift
+//  iHydrateTests
+//
+//  Created by Haya Albaker on 20/10/2024.
+//
+
+import Testing
+@testable import iHydrate
+
+struct iHydrateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
