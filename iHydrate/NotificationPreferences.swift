167672b9b6ae608b5ca26ce84e58949adc419c60
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-10-21 14:56:36 +0300

    text in notif pref

--- iHydrate/NotificationPreferences.swift
+++ iHydrate/NotificationPreferences.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct NotificationPreferences: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @State private var bodyWeight: String = ""
+
+        var body: some View {
+            VStack(alignment: .leading, spacing: 20) {
+
+                Text("Notification Preferences")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text("The start and end hour")
+                    .font(.body)
+                Text("Specify the start and end date to receive the notifications")
+                    .font(.body)
+                    .foregroundColor(.gray)
+
+
+                Spacer()
+                
+                Button(action: {
+                }) {
+                    Text("Start")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.lightBlue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+
+            }
+            .padding()
+            .padding(.top, 100)
+        }}
 
 #Preview {
     NotificationPreferences()
-}
+    }
+
