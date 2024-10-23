9ecb3bbe49221892eeb65652faaf17f4c95e11dd
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-10-23 15:41:56 +0300

    button works!!! wooooo

--- iHydrate/Onboarding.swift
+++ iHydrate/Onboarding.swift
@@ -11,63 +11,65 @@ struct Onboarding: View {
     @State private var bodyWeight: String = ""
     
     var dailyWaterIntake: Double {
-        // Calculate daily water intake based on the body weight
+
         if let weight = Double(bodyWeight) {
-            return weight * 0.03 // Return calculated intake
+            return weight * 0.03
         }
-        return 0.0 // Default value if input is invalid
+        return 0.0
     }
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Image("waterdrop")
-            Text("Hydrate")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            
-            Text("Stay hydrated by recording and tracking your daily water intake based on your needs.")
-                .font(.body)
-                .foregroundColor(.gray)
-            
-            HStack {
-                Text("Body Weight (kg)")
-                    .font(.headline)
+        NavigationView {
+            VStack(alignment: .leading, spacing: 20) {
+                Image("waterdrop")
+                Text("Hydrate")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Text("Stay hydrated by recording and tracking your daily water intake based on your needs.")
+                    .font(.body)
+                    .foregroundColor(.gray)
+                
+                HStack {
+                    Text("Body Weight (kg)")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    TextField("Value", text: $bodyWeight)
+                        .frame(width: 100)
+                        .keyboardType(.decimalPad) // Ensure numeric input
+                        .padding(8)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(5)
+                    
+                    Button(action: {
+                        bodyWeight = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding()
                 
                 Spacer()
                 
-                TextField("Value", text: $bodyWeight)
-                    .frame(width: 100)
-                    .keyboardType(.decimalPad) // Ensure numeric input
-                    .padding(8)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(5)
-                
-                Button(action: {
-                    bodyWeight = ""
-                }) {
-                    Image(systemName: "xmark.circle.fill")
-                        .foregroundColor(.gray)
+                NavigationLink(destination: NotificationPreferences(dailyWaterIntake: dailyWaterIntake)) {
+                    Text("Next")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(bodyWeight.isEmpty ? Color.gray : Color.lightBlue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                        .padding(.top, 20)
                 }
+                .disabled(bodyWeight.isEmpty)
+
             }
             .padding()
-            
-            Spacer()
-            
-            NavigationLink(destination: NotificationPreferences(dailyWaterIntake: dailyWaterIntake)) {
-                Text("Next")
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(bodyWeight.isEmpty ? Color.gray : Color.blue) // Change color based on input
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2) // Add shadow
-                    .padding(.top, 20)
-            }
-            .disabled(bodyWeight.isEmpty)
-
+            .padding(.top, 100)
         }
-        .padding()
-        .padding(.top, 100)
     }
 }
 
